912aa5e1c873b07844c65bde152f18d33ad052d8
Meenal3107 <135640151+Meenal3107@users.noreply.github.com>
2023-09-08 03:27:07 +0530

    Update validate-postal.php

--- validate-postal.php
+++ validate-postal.php
@@ -1,8 +1,10 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
 $country = $_GET['country'];
 $postalCode = $_GET['postal'];
 
-$jsonUrl = 'https://gist.githubusercontent.com/jamesbar2/1c677c22df8f21e869cca7e439fc3f5b/raw/21662445653ac861f8ab81caa8cfaee3185aed15/postal-codes.json';
+$jsonUrl = 'https://raw.githubusercontent.com/Meenal3107/posal-code.json/master/postal-codes.json';
 $jsonData = file_get_contents($jsonUrl);
 
 if ($jsonData === false) {
@@ -20,8 +22,7 @@ foreach ($postalCodeData as $entry) {
 if (isset($countryRegexMap[$country])) {
     $regex = "/" . $countryRegexMap[$country] . "/";
     if (preg_match($regex, $postalCode)) {
-        // echo json_encode(array("status" => "valid", "message" => "Postal code is valid for the selected country."));
-        echo "valid";
+        echo json_encode(array("status" => "valid", "message" => "Postal code is valid for the selected country."));
     } else {
         echo json_encode(array("status" => "invalid", "message" => "Postal code is not valid for the selected country."));
      }
@@ -29,3 +30,4 @@ if (isset($countryRegexMap[$country])) {
     echo json_encode(array("status" => "error", "message" => "Selected country not found in JSON data."));
 }
 ?>
+ 
